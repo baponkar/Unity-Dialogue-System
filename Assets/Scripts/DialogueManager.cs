39ba1691674e1684a0cc829fdb0db26801ead263
Bapon Kar <baponkar600@gmail.com>
2022-08-14 17:07:53 +0530

    All file added

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        //dialogueText.text = sentence;
+        StopAllCoroutines();//stop any coroutines that are running
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End of conversation.");
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unity-Dialogue-System
+
+## Credit : [Brackeys](https://www.youtube.com/watch?v=_nRzoTzeyxU)
